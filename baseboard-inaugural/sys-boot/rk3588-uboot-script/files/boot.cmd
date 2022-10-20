ea032288dd2dfefccb585733d562140bd5bf98b9
yang <yang@fydeos.io>
2022-10-20 13:51:50 +0800

    FTABD-22 unlimit user memory restriction, reduce chrome crash caused by loading a big webpage(>2gb)

--- baseboard-inaugural/sys-boot/rk3588-uboot-script/files/boot.cmd
+++ baseboard-inaugural/sys-boot/rk3588-uboot-script/files/boot.cmd
@@ -5,7 +5,7 @@ if test -e ${devtype} ${devnum}:${distro_bootpart} /boot/first-b.txt; then
   setenv rootpart 5
 fi
 part uuid ${devtype} ${devnum}:${rootpart} root_uuid
-setenv bootargs rootfstype=ext2 rootwait ro cros_debug cros_secure root=PARTUUID=${root_uuid}
+setenv bootargs rootfstype=ext2 rootwait ro cros_debug cros_secure ignore_rlimit_data root=PARTUUID=${root_uuid}
 load ${devtype} ${devnum}:${rootpart} ${kernel_addr_c} ${linux_image}
 load ${devtype} ${devnum}:${rootpart} ${fdt_addr_r} ${fdtfile}
 booti ${kernel_addr_c} - ${fdt_addr_r}
